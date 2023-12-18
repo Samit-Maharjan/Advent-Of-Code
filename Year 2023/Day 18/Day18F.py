7207894788e99af8783fe692bf7077f249b541e7
samit-maharjan <samit.maharjan@cloudera.com>
2023-12-18 13:48:45 +0530

    Optimizing Day 18

--- Year 2023/Day 18/Day18F.py
+++ Year 2023/Day 18/Day18F.py
@@ -1,12 +1,15 @@
 data = open('input.txt', 'r').read().strip().split('\n')
 move = {'R': (0, 1), 'L': (0, -1), 'U': (-1, 0), 'D': (1, 0)}
-x, y, borders = 0, 0, []
+x = y = perimeter = 0
+borders = []
+
 for row in data:
     d, m, _ = row.split()
-    dx, dy = move[d]
-    for _ in range(int(m) ):
-        borders.append( (x, y) )
-        x, y = x + dx, y + dy
+    (dx, dy), m = move[d], int(m)
+    borders.append( (x, y) )
+    perimeter += m
+    x += dx * m
+    y += dy * m
 
 # Shoelace formula
 area = 0
@@ -17,6 +20,6 @@ for i in range(len(borders) - 1):
 area = abs(area) // 2
 
 # Pick's theorem
-perimeter = len(borders)
 interior_area = area - perimeter // 2 + 1
+
 print(interior_area + perimeter)
--- Year 2023/Day 18/Day18S.py
+++ Year 2023/Day 18/Day18S.py
@@ -1,15 +1,17 @@
 data = open('input.txt', 'r').read().strip().split('\n')
 dirs = {'0': 'R', '1': 'D', '2': 'L', '3': 'U'}
 move = {'R': (0, 1), 'L': (0, -1), 'U': (-1, 0), 'D': (1, 0)}
-x, y, borders = 0, 0, []
+x = y = perimeter = 0
+borders = []
+
 for row in data:
     _, _, c = row.split()
     c = c[1:-1]
-    d, m = dirs[c[-1]], int(c[1:-1], 16) 
-    dx, dy = move[d]
-    for _ in range(int(m) ):
-        borders.append( (x, y) )
-        x, y = x + dx, y + dy
+    (dx, dy), m = move[dirs[c[-1]]], int(c[1:-1], 16) 
+    borders.append( (x, y) )
+    perimeter += m
+    x += dx * m
+    y += dy * m
 
 # Shoelace formula
 area = 0
@@ -20,6 +22,6 @@ for i in range(len(borders) - 1):
 area = abs(area) // 2
 
 # Pick's theorem
-perimeter = len(borders)
 interior_area = area - perimeter // 2 + 1
+
 print(interior_area + perimeter)
